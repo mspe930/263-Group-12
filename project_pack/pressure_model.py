Corrected units of pressure in plot [bars] to [MPa]

project_pack/pressure_model.py
@@ -3,7 +3,7 @@ from scipy import interpolate
 from scipy.interpolate import interp1d
 from matplotlib import pyplot as plt
 
-def pressure_ode(t, P, q, dqdt, P0, a, b, c):
+def pressure_ode(t, P, q, dqdt, P0, a, b, c, d):
     ''' Returns time derivative of reservoir pressure, dP/dt, for given parameters.
 
         Parameters
@@ -24,6 +24,8 @@ def pressure_ode(t, P, q, dqdt, P0, a, b, c):
             Recharge strength lumped parameter.
         c : float
             Slow drainage strength lumped parameter.
+        d : float
+            Diffusion strength lumped parameter.
     
         Returns
         -------
@@ -172,14 +174,15 @@ if __name__ == "__main__":
     a = 8.e-5
     b = 1.e-2
     c = 7.e-3
+    d = 1.e-1
 
-    ts_model,Ps_model = solve_pressure_ode(f=pressure_ode,t0=tmin,t1=tmax,dt=0.05,P0=P0,pars=[a,b,c])
+    ts_model,Ps_model = solve_pressure_ode(f=pressure_ode,t0=tmin,t1=tmax,dt=0.05,P0=P0,pars=[a,b,c,d])
 
     f,ax = plt.subplots(1,1)
     ax.plot(Ps_data[0,:],Ps_data[1,:],'kx',label='Measured Data')
     ax.plot(ts_model,Ps_model,'r-',label='Fitted Model')
     ax.set_xlabel('Year of observation [A.D.]')
-    ax.set_ylabel('Reservoir pressure [bars]')
+    ax.set_ylabel('Reservoir pressure [MPa]')
     ax.legend()
     ax.set_title('Comparison of measured pressure and modelled pressure over time in the Ohaaki geothermal reservoir')
     plt.show()