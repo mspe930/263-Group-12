0a8022b462c1d219286a0441e6a47911816e2a80
John Cole <john@johncole.nz>
2021-09-03 19:25:49 +1200

    Added some callibration

--- project_pack/pressure_model.py
+++ project_pack/pressure_model.py
@@ -72,28 +72,6 @@ def interpolate_mass_flow(ts):
     # returns vector of net mass flow rates
     return qs
 
-def interpolate_pressure(ts):
-    ''' Reads injection and production rates, interpolates this data, and calculates
-        the pressure in the reservoir. 
-
-        Parameters
-        ----------
-        ts : array-like
-            Vector of times to interpolate.
-        
-        Returns
-        -------
-        qs : array-like
-            Vector of pressures for given times. 
-    '''
-    pressure_data = np.genfromtxt('data_sources/cs_p.txt',dtype=float,delimiter=', ',skip_header=1).T
-
-    # interpolates injection rates for given times
-    fp = interp1d(pressure_data[0,:], pressure_data[1,:], kind='linear', fill_value=(0.,0.), bounds_error=False)
-    fp_fit = fp(ts)
-
-    return fp_fit 
-
 def compute_dqdt(ts, qs):
     ''' Computes the time derivative of net mass flow rates, dq/dt. 
 
@@ -258,8 +236,8 @@ if __name__ == "__main__":
     #theta = [0.00028498, 0.00101189, 0.00599222]
     #theta = [0, 1.e-3, 6.e-3]
     #theta = [0.0002892, 0.00122505, 0.00517294]
-    #theta = [-2.45714621e-05, 1.15000715e-02, 4.20912094e-03]
-    theta = [-1.96091016e-05, 1.14407243e-02, 3.4755337e-03]
+    theta = [-2.45714621e-05, 1.15000715e-02, 4.20912094e-03]
+    #theta = [-1.96091016e-05, 1.14407243e-02, 3.4755337e-03]
     #theta = [2, 1.14407243, 3.4755337]
     
     # Trying to permutate into global minima
