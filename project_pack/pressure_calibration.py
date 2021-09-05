bcc6dcb6d1066461e84a00514a10bc32c777620e
ehat114 <81003235+ehat114@users.noreply.github.com>
2021-09-06 11:12:51 +1200

    Added pressure_model function to solve for pressure using our imported model

--- project_pack/pressure_calibration.py
+++ project_pack/pressure_calibration.py
@@ -4,8 +4,18 @@ from scipy.optimize import curve_fit
 from scipy.interpolate import interp1d
 from pressure_model import *
 
+def pressure_model(t,M0,a,b,c,d,P0):
+    pars = np.array([M0,a,b,c,d,P0])
+    ts,Ps = solve_pressure_ode(f=pressure_ode,t0=ts_data[0],t1=t[-1],dt=0.2,P0=Ps_data[0],pars=pars)
+    f = interp1d(ts,Ps,kind='linear',bounds_error=False)
+    Ps = f(t)
+    return Ps
+
 def main():
-    pass
+    global ts_data, Ps_data
+    data = np.genfromtxt('cs_p.txt',dtype=float,delimiter=', ',skip_header=1).T
+    ts_data = data[0,:]
+    Ps_data = data[1,:]
 
 if __name__ == "__main__":
     main()
