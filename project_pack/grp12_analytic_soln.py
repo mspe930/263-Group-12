b63d17fbdfe1a2c7dd464e6fb769e43bcea770bb
bjon638 <80793829+bjon638@users.noreply.github.com>
2021-09-07 14:31:27 +1200

    Add files via upload
    Added concentration benchmarking

--- project_pack/grp12_analytic_soln.py
+++ project_pack/grp12_analytic_soln.py
@@ -6,59 +6,65 @@ from numpy.lib.npyio import genfromtxt, load
 
 
 def plot_benchmark():
-    from_data = np.genfromtxt("cs_p.txt", delimiter=',', skip_header=1)
-    length = shape(from_data)
+    press_data = np.genfromtxt("cs_p.txt", delimiter=',', skip_header=1)
+    length = shape(press_data)
 
-    t0 = from_data[0, 0]
-    # period_meas = from_data[-1, 0] - from_data[0, 0]
-    #tot_seconds = period_meas*365*24*3600
-    t = np.linspace(t0, from_data[-1, 0], num=length[0])
+    t0 = press_data[0, 0]
+    t = np.linspace(t0, press_data[-1, 0], num=length[0])
 
     p_ana = np.zeros(length[0])
     a = 2*(10 ** (-1))
     b = 1*(10 ** (-1))
-    P0 = from_data[0, 1]
+    P0 = press_data[0, 1]
 
     for i in range(len(t)):
         time = (t[i] - t0)  # *365*24*3600
-        print(time)
         p_ana[i] = ((-a/b)*(1-(np.exp(-b*time))) + P0)
 
     f, ax = plt.subplots(nrows=1, ncols=1)
-    ax.plot(from_data[:, 0], from_data[:, 1], 'r.', label='given data')
+    ax.plot(press_data[:, 0], press_data[:, 1],
+            'r.', label='given pressure data')
     ax.plot(t, p_ana, 'b', label="analytical")
-    ax.set_ylabel("Pressure")
+    ax.set_ylabel("Pressure [MPa]")
     ax.set_xlabel("Year")
     ax.legend()
 
-    # plt.show()
+    plot = True
+    if plot:
+        plt.show()
+    else:
+        plt.savefig('263_press_benchmark.png', dpi=300)
 
     conc_data = np.genfromtxt("cs_cc.txt", delimiter=',', skip_header=1)
-    size = shape(conc_data)
+    length = shape(conc_data)
 
     t0 = conc_data[0, 0]
-    # period_meas = from_data[-1, 0] - from_data[0, 0]
-    #tot_seconds = period_meas*365*24*3600
-    t = np.linspace(t0, conc_data[-1, 0], num=size[0])
+    t = np.linspace(t0, conc_data[-1, 0], num=length[0])
 
-    c_ana = np.zeros(size[0])
-    a = 1*(10 ** (-3))
-    b = 1*(10 ** (-1))
-    P0 = from_data[0, 1]
+    c_ana = np.zeros(length[0])
+
+    M0 = 8.3 * 10 ** 3
+    C0 = conc_data[0, 1]
+    k = 10 / M0
+    d = 0.5 * 10 ** (-1)
+    L = (k * conc_data[0, 1] - k) / (k + d)
 
     for i in range(len(t)):
-        time = (t[i] - t0)  # *365*24*3600
-        print(time)
-        c_ana[i] =
+        time = (t[i] - t0)  # * 365 * 24  # *3600
+        c_ana[i] = ((k + d*C0) / (k + d)) + (L / np.exp((k+d)*time))
 
     f, ax = plt.subplots(nrows=1, ncols=1)
-    ax.plot(conc_data[:, 0], conc_data[:, 1], 'r.', label='given data')
-    # ax.plot(t, p_ana, 'b', label="analytical")
-    ax.set_ylabel("solution of conc.")
-    ax.set_xlabel("year")
+    ax.plot(conc_data[:, 0], conc_data[:, 1],
+            'r.', label='given concentration data')
+    ax.plot(t, c_ana, 'b', label="analytical")
+    ax.set_ylabel("CO2 Concentration [wt%]")
+    ax.set_xlabel("Year")
     ax.legend()
 
-    plt.show()
+    if plot:
+        plt.show()
+    else:
+        plt.savefig('263_concentration_benchmark.png', dpi=300)
 
 
 if __name__ == "__main__":
