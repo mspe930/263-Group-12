222d5afd1923a6e14d9cd540d6a23503ff457d07
ehat114 <81003235+ehat114@users.noreply.github.com>
2021-09-08 16:45:49 +1200

    Added function to plot conc. residuals

--- project_pack/concentration_model/concentration_calibration.py
+++ project_pack/concentration_model/concentration_calibration.py
@@ -3,39 +3,61 @@ from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 from scipy.interpolate import interp1d
 from concentration_model import *
-import pressure_calibration
+from pressure_calibration import calibrate_pressure_model
 
 def concentration_model(t,M0,a,b,c,d,P0):
+
     pars = np.array([M0,a,b,c,d,P0])
+    ts_data,Cs_data = fetch_concentration_data()
     ts,Cs = solve_concentration_ode(f=concentration_ode,t0=ts_data[0],t1=t[-1],dt=0.2,C0=Cs_data[0],pars=pars)
     f = interp1d(ts,Cs,kind='linear',bounds_error=False)
     Cs = f(t)
     return Cs
 
-def main():
-    global ts_data, Cs_data
-    data = np.genfromtxt('cs_cc.txt',dtype=float,delimiter=', ',skip_header=1).T
-    ts_data = data[0,:]
-    Cs_data = data[1,:]
+def compute_residuals(Cs):
+    ts_data,Cs_data = fetch_concentration_data()
+    residuals = np.zeros(len(ts_data))
+    for i in range(len(ts_data)):
+        residuals[i] = Cs[i] - Cs_data[i]
+    return residuals
 
-    # calibrate a, b, c and P0 using the pressure calibration script
-    a,b,c,P0 = pressure_calibration.main()
-    # initial parameter guess
-    pars0 = [5.e+03, a, b, c, 5.e-01, P0]
+def plot_concentration_residuals(pars):
+    ts_data,Cs_data = fetch_concentration_data()
+    ts,Ps = solve_concentration_ode(f=concentration_ode,t0=ts_data[0],t1=ts_data[-1],dt=0.2,C0=Cs_data[0],pars=pars)
+    f = interp1d(ts,Ps,kind='linear',bounds_error=False)
+    Cs = f(ts_data)
+    residuals = compute_residuals(Cs)
 
-    # bounding radii for parameters
-    # a, b, c are already calibrated and P0 is fixed so all of these have a very small radius
-    bounding_radius = [10e8, .001*a, .001*b, .001*c, 1.e00, .001*P0]
-    # set bounding intervals for parameters
-    pars_bounds = ([sum(x) for x in zip(pars0,[-1*e for e in bounding_radius])], [sum(x) for x in zip(pars0,bounding_radius)] )
+    f,ax = plt.subplots(1,1)
+    ax.plot(ts_data,residuals,'ro',markerSize=8,markerfacecolor='none',label='Fitted Model:\n M0 = {:1.2e}\n a = {:1.2e}\n b = {:1.2e}\n c = {:1.2e}\n d = {:1.2e}'.format(*pars[:-1]))
+    ax.set_xlabel('Year of observation [A.D.]')
+    ax.set_ylabel('Residuals [MPa]')
+    ax.set_title('Residuals plot of fitted LP CO2 concentration model')
+    ax.legend()
+    plt.show()
 
-    p,_ = curve_fit(concentration_model,ts_data,Cs_data,p0=pars0,bounds=pars_bounds)
 
+def calibrate_concentration_model(pars0):
+    ts_data,Cs_data = fetch_concentration_data()
+    p = calibrate_pressure_model(pars0)
+    lwr_bnds = [0., .999*p[1], .999*p[2], .999*p[3], 0., .999*p[5]]
+    upr_bnds = [10.e10, 1.001*p[1], 1.001*p[2], 1.001*p[3], 10.e10, 1.001*p[5]]
+    bounds = (lwr_bnds, upr_bnds)
+
+    p,_ = curve_fit(concentration_model,ts_data,Cs_data,p0=p,bounds=bounds)
     print("\nAdditional parameter estimations for concentration model:")
     print(" "*5, end="")
     print("d = {:1.2e}".format(p[4]))
     print(" "*4, end="")
     print("M0 = {:1.2e}\n".format(p[0]))
+
+    return p
+
+def main():
+    pars0 = [8.e+03, 2.5e-3,  3.e-01, 8.e-04, 5.e-01,  6.17e+00]
+    pars = calibrate_concentration_model(pars0)
+    plot_concentration(pars)
+    plot_concentration_residuals(pars)
 
 if __name__ == "__main__":
     main()
