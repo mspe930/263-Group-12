aebb834e9869f0faaafc221f7f3f6617a7a160bd
ehat114 <81003235+ehat114@users.noreply.github.com>
2021-09-09 19:40:47 +1200

    deleted unneeded plots (manually calibrated models)

--- project_pack/main.py
+++ project_pack/main.py
@@ -9,15 +9,28 @@ def main():
     # initial guess of parameters
     pars0 = [5.e+03, 2.5e-3,  3.e-01, 8.e-04, 5.e-01,  6.17e+00]
 
+    '''
+    Don't think we need these plots.
+    '''
     # plot initial pressure model
-    pressure_model.plot_pressure(pars0)
-    pressure_calibration.plot_pressure_residuals(pars0)
+    #pressure_model.plot_pressure(pars0)
+    #pressure_calibration.plot_pressure_residuals(pars0)
     # plot initial concentration model
-    concentration_model.plot_concentration(pars0)
-    concentration_calibration.plot_concentration_residuals(pars0)
+    #concentration_model.plot_concentration(pars0)
+    #concentration_calibration.plot_concentration_residuals(pars0)
 
     # calibrate model to fit best fit parameters
-    pars = concentration_calibration.calibrate_concentration_model(pars0)
+    # with slow drainage initially zero i.e. c = 0
+    pars,_ = concentration_calibration.calibrate_concentration_model(pars0)
+    # slow drainage c = 0
+    pars[3] = 0.
+    pressure_model.plot_pressure(pars)  # pressure plot
+    pressure_calibration.plot_pressure_residuals(pars) # pressure residual plot
+    concentration_model.plot_concentration(pars)    # concentration plot
+    concentration_calibration.plot_concentration_residuals(pars) # concentration residual plot
+
+    # recallibrate model with slow drainage
+    pars,_ = concentration_calibration.calibrate_concentration_model(pars0)
     # plot best fit pressure model
     pressure_model.plot_pressure(pars)
     pressure_calibration.plot_pressure_residuals(pars)
@@ -29,6 +42,8 @@ def main():
     pressure_predictions.plot_predictions(pars)
     # plot what-ifs of concentration model
     concentration_predictions.plot_predictions(pars)
+
+    
 
 
 if __name__ == "__main__":
